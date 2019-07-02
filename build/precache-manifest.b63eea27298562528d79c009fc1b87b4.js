b2665fc74a8f238cb16b8ac56a0553501c7b5ba7
Boots2291 <buteramv@gmail.com>
2019-07-02 13:02:56 -0400

    uibuild

---
+++ build/precache-manifest.b63eea27298562528d79c009fc1b87b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3438173d003b66722dbf1f9d51a5366a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd912f6b496a3cbe88e5",
+    "url": "/static/css/main.91b31c34.chunk.css"
+  },
+  {
+    "revision": "bc120ca277fc438795d3",
+    "url": "/static/js/2.dbf5eb8f.chunk.js"
+  },
+  {
+    "revision": "dd912f6b496a3cbe88e5",
+    "url": "/static/js/main.1e76b77b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
